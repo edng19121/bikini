fda41d0b41fdca738e77df3a8237c6c277628d66
Trung <trunganh13@gmail.com>
2021-08-26 19:09:15 +0700

    add new files

---
+++ menu.js
@@ -0,0 +1,11 @@
+const images=document.querySelectorAll(".list img")
+
+images.forEach(el => el.addEventListener("click", handleChangeImage));
+
+function handleChangeImage(e){
+       const source = e.target.getAttribute("src");
+
+       const featureImage=document.querySelector(".feature-image")
+
+       featureImage.setAttribute("src", source);
+}
